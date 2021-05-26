7b31ffba45ad6d8f42c22fae59804f4c4f0a43ae
djlolepop <67583626+djlolepop@users.noreply.github.com>
2021-05-26 18:01:15 +0530

    Add files via upload

---
+++ clsstudent.py
@@ -0,0 +1,20 @@
+class Student(object):
+    def __init__(self, name, age, gender, level, grades=None):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.level = level
+        self.grades = grades or {}
+
+    def setGrade(self, course, grade):
+        self.grades[course] = grade
+
+    def getGrade(self, course):
+        return self.grades[course]
+
+    def getGPA(self):
+        return sum(self.grades.values())/len(self.grades)
+
+# Define some students     
+jatin = Student("jatin", 15, "male", 8,{"math":5.5})
+print(jatin.getGPA())
